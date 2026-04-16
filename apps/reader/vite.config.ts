6155a637e8a0136e5cc97a9f714a6ab27c7dcb1a
ringyuki <hikarinagi@outlook.com>
2026-04-16 18:06:09 +0800

    chore: add Vite and vite-tsconfig-paths dependencies, update TypeScript configuration for improved path resolution

--- apps/reader/vite.config.ts
+++ apps/reader/vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'node:path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: process.env['GITHUB_ACTIONS'] ? '/Rito/' : '/',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
